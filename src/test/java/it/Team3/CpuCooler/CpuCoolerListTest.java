b979e6f68407501e942b1565df61910822f1da7f
Giuseppe <tasso96@gmail.com>
2023-09-29 16:14:12 +0200

    junit

---
+++ src/test/java/it/Team3/CpuCooler/CpuCoolerListTest.java
@@ -0,0 +1,11 @@
+package it.Team3.CpuCooler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CpuCoolerListTest {
+    
+    @org.junit.jupiter.api.Test
+    void getCpuCoolerList() {
+        assertEquals(true, true);
+    }
+}
