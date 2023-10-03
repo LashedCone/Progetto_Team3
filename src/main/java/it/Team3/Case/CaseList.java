ea80bffc9c1c2075f663a7cf86094157b9edff30
Giuseppe <tasso96@gmail.com>
2023-10-03 11:07:32 +0200

    modifiche oggetti case

--- src/main/java/it/Team3/Case/CaseList.java
+++ src/main/java/it/Team3/Case/CaseList.java
@@ -19,11 +19,11 @@ public class CaseList {
             "USB 3.0 × 2", "Micro ATX, Mini ITX", "360 mm / 14.17",
             1, 2, 4, "120mm × 2");
     
-    Case phanteks = new Case("Phanteks", "Enthoo Pro M", "ATX Mid Tower", "Black",
+    Case phanteksEnthooProM = new Case("Phanteks", "Enthoo Pro M", "ATX Mid Tower", "Black",
             "USB 3.1 Gen 2 Type-C, USB 3.0 × 2", "ATX, Micro ATX, Mini ITX", "406 mm / 16.0",
             2, 2, 7, "360mm × 1 or 280mm × 1");
     
-    Case nzxtH710i = new Case("NZXT", "", "ATX Mid Tower", "White",
+    Case nzxtH710i = new Case("NZXT", "H7i10", "ATX Mid Tower", "White",
             "USB 3.1 Gen 2 Type-C, USB 3.1 Gen 1 Type-A", "ATX, EATX, Micro ATX, Mini ITX", "413 mm / 16.3",
             7, 2, 7, "360mm × 1 or 280mm × 1");
     
@@ -31,7 +31,7 @@ public class CaseList {
             "USB 3.1 Gen 2 Type-C, USB 3.1 Gen 1 Type-A", "ATX, EATX, Micro ATX, Mini ITX", "440 mm / 17.3",
             6, 2, 7, "360mm × 1 or 280mm × 1");
     
-    Case corsair = new Case("Corsair", "Crystal Series 680X", "ATX Mid Tower", "Black",
+    Case corsairC680x = new Case("Corsair", "Crystal Series 680X", "ATX Mid Tower", "Black",
             "USB 3.1 Gen 2 Type-C, USB 3.0 × 2", "ATX, EATX, Micro ATX, Mini ITX", "330 mm / 13.0",
             3, 2, 8, "360mm × 2 or 280mm × 2");
     
@@ -42,10 +42,10 @@ public class CaseList {
         CaseList.add(nzxtH510i);
         CaseList.add(fractalDesignMeshifyC);
         CaseList.add(coolerMaster);
-        CaseList.add(phanteks);
+        CaseList.add(phanteksEnthooProM);
         CaseList.add(nzxtH710i);
         CaseList.add(fractalDesignDefineR6);
-        CaseList.add(corsair);
+        CaseList.add(corsairC680x);
     }
     
     public ArrayList<Case> getCaseList() {
