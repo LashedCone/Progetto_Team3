868eb09eb2c010de047d155813a66333e4d3a3a9
Alex <alex97av@gmail.com>
2023-10-05 15:28:08 +0200

    remove user,final_result.sql file , add sqlessentials package and sqlstarter.sql

---
+++ src/main/sql/it.Team3.sqlessentials/SQLStarter.sql
@@ -0,0 +1,24 @@
+--creazione ed utilizzo del database
+CREATE DATABASE IF NOT EXISTS ProjectTeam3;
+USE ProjectTeam3;
+--Creazione user list
+CREATE TABLE IF NOT EXISTS user(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+--creazione tabella finale
+CREATE TABLE IF NOT EXISTS final_result (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    User VARCHAR(255),
+    Cpu VARCHAR(255),
+    Cpucooler VARCHAR(255),
+    Motherboard VARCHAR(255),
+    Memory VARCHAR(255),
+    Storage VARCHAR(255),
+    Gpu VARCHAR(255),
+    Casepc VARCHAR(255),
+    Power_supply VARCHAR(255),
+    FOREIGN KEY (User) REFERENCES user (Username)
+);
