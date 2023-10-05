8e873de6e83d1416e03c3b81a7ff05adcfd06b79
Giuseppe <tasso96@gmail.com>
2023-10-05 18:17:13 +0200

    classe astratta componentÂ

---
+++ src/main/java/it/Team3/componentfile/Component.java
@@ -0,0 +1,19 @@
+package it.Team3.componentfile;
+
+public abstract class Component {
+    private String name;
+    private double price;
+    
+    public Component(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+}
