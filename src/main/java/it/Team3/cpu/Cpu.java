24f35ee1e6ceeeddbd4886e00f1baec21230b744
Giuseppe <136367030+LashedCone@users.noreply.github.com>
2023-10-05 21:38:56 +0200

    Update Cpu.java

--- src/main/java/it/Team3/cpu/Cpu.java
+++ src/main/java/it/Team3/cpu/Cpu.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:src/main/java/it/Team3/cpu/Cpu.java
 package it.Team3.cpu;
 
-=======
-package it.Team3.Cpu;
->>>>>>> d8b1ddac8e8c3cf6383528555b5f8dff2a4e963e:src/main/java/it/Team3/Cpu/Cpu.java
-=======
-package it.Team3.cpu;
->>>>>>> 2d79513ecfec0896bf10c5f2496ecf8397ec1ed5
 public record Cpu(String manufacturer,
                   String part,
                   int coreCount,
