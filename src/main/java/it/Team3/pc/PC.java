86cd71b1c6fb601aa38df6b7bd4f6c9001d8f945
Giuseppe <tasso96@gmail.com>
2023-10-07 12:09:28 +0200

    svilÃuppo classe PC

--- src/main/java/it/Team3/pc/PC.java
+++ src/main/java/it/Team3/pc/PC.java
@@ -1,5 +1,28 @@
 package it.Team3.pc;
 
-public class PC {
+import it.Team3.casefile.Case;
+import it.Team3.componentfile.Component;
+import it.Team3.cpu.Cpu;
+import it.Team3.cpucooler.CpuCooler;
+import it.Team3.gpu.Gpu;
+import it.Team3.motherboard.Motherboard;
+import it.Team3.powersupply.PowerSupply;
+import it.Team3.ram.RAM;
+import it.Team3.storagefile.Storage;
 
+import java.util.List;
+
+public class PC {
+    private List<Component> components;
+    
+    public PC(Cpu cpu, CpuCooler cpuCooler, Motherboard motherboard,
+              RAM ram, Storage storage, Gpu gpu, Case containerCase, PowerSupply powerSupply) {
+        
+    }
+    
+    public void addComponent(Cpu cpu, CpuCooler cpuCooler, Motherboard motherboard,
+                             RAM ram, Storage storage, Gpu gpu, Case containerCase, PowerSupply powerSupply){
+    PC pc = new PC(cpu, cpuCooler, motherboard, ram, storage, gpu, containerCase, powerSupply);
+    
+    }
 }
