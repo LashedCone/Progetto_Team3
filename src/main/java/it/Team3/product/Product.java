fa44a7bc671c0e40260b45feda7a6000404e166b
Giuseppe <tasso96@gmail.com>
2023-10-15 23:44:48 +0200

    creata classe product

---
+++ src/main/java/it/Team3/product/Product.java
@@ -0,0 +1,46 @@
+package it.Team3.product;
+
+import it.Team3.exception.MyException;
+import it.Team3.utilities.Components;
+
+public class Product {
+    private Components components;
+    private String name;
+    private String manufacturer;
+    private double price;
+    
+    public Components getComponents() {
+        return components;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setComponents(Components components) {
+        this.components = components;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    
+    public void setPrice(double price) throws MyException {
+        if(price < 0) {
+            throw new MyException("Il prezzo non può essere minore di 0");
+        }
+        this.price = price;
+    }
+}
