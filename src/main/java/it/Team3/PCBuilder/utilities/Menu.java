b12008dc3adaf5bdcb56bf27409611b8177d623f
Alex <alex97av@gmail.com>
2023-10-20 21:58:32 +0200

    added UserMenu, implemented UserMenu in Menu

--- src/main/java/it/Team3/PCBuilder/utilities/Menu.java
+++ src/main/java/it/Team3/PCBuilder/utilities/Menu.java
@@ -9,31 +9,39 @@ public class Menu {
     public static void Menu() throws MyException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Benvenuto!");
-        String[] options = {
-                "Create user",
-                "Login",
-                "Exit"
-        };
-        System.out.println("Selezionare un opzione:");
-        for (int i = 0; i < options.length; i++) {
-            System.out.println(i + 1 + " " + options[i]);
-        }
-        int option = scanner.nextInt();
-        switch (option) {
-            case 1 -> {
-                UserRegistration.creationUser();
-                String user = UserCheck.userCheck();
-                ComponentSelection.componentSelection(user);
+        while (true) {
+            String[] options = {
+                    "Create user",
+                    "Login",
+                    "Exit"
+            };
+            System.out.println("Selezionare un opzione:");
+            for (int i = 0; i < options.length; i++) {
+                System.out.println(i + 1 + " " + options[i]);
             }
-            case 2 -> {
-                String user = UserCheck.userCheck();
-                ComponentSelection.componentSelection(user);
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1 -> {
+                    UserRegistration.creationUser();
+//                String user = UserCheck.userCheck();
+//                ComponentSelection.componentSelection(user);
+                }
+                case 2 -> {
+                    {
+                        String user = UserCheck.userCheck();
+                        UserMenu.userMenu(user);
+                    }
+                }
+                case 3 -> {
+                    System.out.println("Fine!");
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println("Errore:opzione non valida!");
+
+                }
+
             }
-            case 3 -> {
-                System.out.println("Fine!");
-                System.exit(0);
-            }
-            default -> System.out.println("Errore:opzione non valida!");
         }
     }
 }
