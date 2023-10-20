c74b3d587430f54831f8b5356d1202c491452451
Alex <alex97av@gmail.com>
2023-10-20 16:13:50 +0200

    add Menu method

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import it.Team3.PCBuilder.exception.MyException;
+
+import static it.Team3.PCBuilder.utilities.Menu.Menu;
+
+public class Main {
+    public static void main(String[] args) throws MyException {
+        Menu();
+    }
+}
